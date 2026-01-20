7ced3b5e909f9f6c11f9704286dac49dc8a09aef
Saswat <mohantysaswat154@gmail.com>
2026-01-20 09:39:27 +0530

    using data from a loader in the route component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,21 +1,12 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Event 1" },
-  { id: "e2", title: "Event 2" },
-];
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
+  const events = useLoaderData();
   return (
     <>
-      <h1>Events Page</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
 }
