a85204f94a84271e4e8ef765b3a381a7168df5a8
Saswat <mohantysaswat154@gmail.com>
2026-01-23 10:55:05 +0530

    fixing bug on Event List

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -8,8 +8,8 @@ function EventsList({ events }) {
       <h1>All Events</h1>
       <ul className={classes.list}>
         {events.map((event) => (
-          <li key={`/events/${event.id}`} className={classes.item}>
-            <Link to={event.id}>
+          <li key={event.id} className={classes.item}>
+            <Link to={`/events/${event.id}`} className={classes.link}>
               <img src={event.image} alt={event.title} />
               <div className={classes.content}>
                 <h2>{event.title}</h2>
